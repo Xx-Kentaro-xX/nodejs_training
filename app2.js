3fc75a3bba7ed37f0782855893d3f2756484c3bf
Xx-Kentaro-xX <kentravel0328@gmail.com>
2024-10-25 22:14:10 +0900

    app2.js has been customized

--- app2.js
+++ app2.js
@@ -1,18 +1,19 @@
-const http = require('http');
+const http = require("http");
 
-var server = http.createServer(
-  (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.write('<!DOCTYPE html><html lang="ja">');
-    response.write('<head><meta charset="utf-8">');
-    response.write('<title>Hello</title></head>');
-    response.write('<body><h1>Hello Node.js!</h1>');
-    response.write('<p>This is Node.js sample page.</p>');
-    response.write('<p>これは、Node.jsのサンプルページです。</p>', 'utf8');
-    response.write('</body></html>');
-    response.end();
-  }
-);
+var server = http.createServer((request, response) => {
+  response.setHeader("Content-Type", "text/html");
+  response.write('<!DOCTYPE html><html lang="ja">');
+  response.write('<head><meta charset="utf-8">');
+  response.write("<title>Hello</title></head>");
+  response.write("<body><h1>Hello Node.js!</h1>");
+  response.write("<p>This is My First Node.js sample page.</p>");
+  response.write(
+    "<p>私が作った初めてのNode.jsのサンプルページです。</p>",
+    "utf8"
+  );
+  response.write("</body></html>");
+  response.end();
+});
 
 server.listen(3000);
-console.log('Server start!');
+console.log("Server start!");
